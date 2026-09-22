773164895801f55f137b72dacc1bad8d9949c0c8
Jorge Hashiguchi <jorge.hashiguchi2005@gmail.com>
2026-09-22 16:50:27 -0300

    feat: add initial navbar

---
+++ components/layout/navbar.tsx
@@ -0,0 +1,14 @@
+export function Navbar() {
+  return (
+    <nav>
+      <a href="/">Jorge Massaru</a>
+
+      <div>
+        <a href="#sobre">Sobre</a>
+        <a href="#projetos">Projetos</a>
+        <a href="#experiencia">Experiência</a>
+        <a href="#contato">Contato</a>
+      </div>
+    </nav>
+  );
+}
